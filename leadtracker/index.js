0625e2555e744eb878d512dac6c232ce1a94dd55
Anagha <anaghamv012@gmail.com>
2024-07-16 08:29:34 +0530

    leadtracker

---
+++ leadtracker/index.js
@@ -0,0 +1,61 @@
+let myLeads = []
+let oldLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const tabBtn = document.getElementById("tab-btn")
+// let name = localStorage.getItem("myName")
+// let name = localStorage.getItem("myName")
+// console.log(name)
+// localStorage.clear()
+// console.log(leadsFromLocalStorage)
+
+if (leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+const tabs = [
+    {url: "https://www.linkedin.com/in/per-harald-borgen/"}
+]
+
+tabBtn.addEventListener("click", function(){
+    // console.log(tabs[0].url)
+    this.compareDocumentPosition.tabs.query({active:true, currentWindow: true}, function(tabs){
+        let activeTab = tabs[0]
+        let activeTabId = activeTab.id
+    })
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+})
+
+function render(leads){
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++){
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
+deleteBtn.addEventListener("dblclick", function(){
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+inputBtn.addEventListener("click", function(){
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+    // console.log(localStorage.getItem("myLeads", JSON.stringify(myLeads)))
+
+})
