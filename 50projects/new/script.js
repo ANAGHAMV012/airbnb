87c0d22c4ca0d65d4dedd051825f28114b1c33c4
Anagha <anaghamv012@gmail.com>
2024-08-12 09:27:34 +0530

    50projects

---
+++ 50projects/new/script.js
@@ -0,0 +1,9 @@
+const rows = document.querySelector('.row')
+const creation = document.querySelector('.circle')
+rows.addEventListener('click', () => {
+    // console.log('printed')
+    if (str.style.display === "none") {
+        return x.style.display = "block";
+        document.querySelector('.circle.active')
+      }
+})
